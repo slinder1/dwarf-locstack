dad5585561b78d66f73db0e9b40b8fa8c4a51ccb
Scott Linder <Scott.Linder@amd.com>
2025-09-10 15:52:08 -0400

    Add web-based playground in js/
    Use js_of_ocaml and some ppx-based syntax extensions to derive a
    web-based playground for Dwarf_locstack.
    
    Minimal changes were required to the original module source, the biggest
    being a rename from dwarf-locstack to dwarf_locstack to make it a valid
    module identifier. The remaining changes are just to satisfy some
    warnings that default to errors. I masked out some of these with
    compiler options, but fixed a couple in the source.

---
+++ js/dwarf_locstack_sexp.ml
@@ -0,0 +1,14 @@
+open Sexplib.Std
+
+(* Re-open relevant types in Dwarf_locstack to derive sexp functions *)
+type data = [%import: Dwarf_locstack.data]
+and context_item = [%import: Dwarf_locstack.context_item]
+and storage = [%import: Dwarf_locstack.storage]
+and location = [%import: Dwarf_locstack.location]
+and stack_element = [%import: Dwarf_locstack.stack_element]
+and dwarf_op = [%import: Dwarf_locstack.dwarf_op]
+[@@deriving sexp]
+
+(* Add some types not present in Dwarf_locstack but useful with sexp *)
+type context_t = (context_item list) [@@deriving sexp]
+type locexpr_t = (dwarf_op list) [@@deriving sexp]
